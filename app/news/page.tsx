49cb6f77b3a05cc89d5d7ad55d39dbc7b7b122fe
Luffy <w24002@osfl.ac.jp>
2025-05-22 12:09:53 +0900

    7章まで完了

---
+++ app/news/page.tsx
@@ -0,0 +1,18 @@
+import { getNewsList } from "@/app/_libs/microcms";
+import NewsList from "@/app/_components/NewsList";
+import Pagination from "../_components/Pagination";
+import { NEWS_LIST_LIMIT } from "@/app/_constants";
+import SearchField from "../_components/SearchField";
+
+export default async function Page() {
+  const { contents: news, totalCount } = await getNewsList({
+    limit: NEWS_LIST_LIMIT,
+  });
+  return (
+    <>
+      <SearchField />
+      <NewsList news={news} />
+      <Pagination totalCount={totalCount} />
+    </>
+  );
+}
